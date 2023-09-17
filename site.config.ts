e9a5684a64e432fe7ac74f4ce13b04c0bd19b514
AINMNV0023 <144583148+AINMNV0023@users.noreply.github.com>
2023-09-17 20:36:00 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '17108fcb32a4480db4f8aaaae14ac483',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'flowercloud帮助中心',
+  domain: 'fc.i3.wtf',
+  author: 'i3.wtf',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
